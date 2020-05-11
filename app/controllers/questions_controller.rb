a6f832191f30963baef65e6aaa07c6b9d8ad62b7
Augustin Lazarus <augustin.lazarus@free.fr>
2020-05-11 12:48:17 +0200

    2nd challenge done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question] == 'I am going to work'
+      @answers = 'Great!'
+    elsif params[:question].end_with?('?')
+      @answers = 'Silly question, get dressed and go to work!'
+    else
+      @answers = "I don't care, get dressed and go to work!"
+    end
+  end
+end
